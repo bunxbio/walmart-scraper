1bac1d1c730a2aa6797ab08a50b759288429e0c6
bunxbio <biokenfigumilang@gmail.com>
2021-08-24 00:38:11 +0700

    modify seeder

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -1,5 +1,5 @@
 pages << {
-  url: 'https://www.walmart.com/browse/movies-tv-shows/4096?facet=new_releases:Last+90+Days',
+  url: 'https://www.walmart.com/browse/new-in-movies/0/0/?_refineresult=true&_be_shelf_id=1522545&search_sort=100&facet=shelf_id:1522545',
   page_type: "listings",
   fetch_type: 'browser',
   force_fetch: true,
@@ -7,8 +7,5 @@ pages << {
   headers: {"User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"},
 #  driver: {
 #    code: "await page.evaluate('window.scrollBy(0,1200)'); await sleep(1000); await page.evaluate('window.scrollBy(0,1200)'); await sleep(1000);await page.evaluate('window.scrollBy(0,1200)'); await sleep(1000);await page.evaluate('window.scrollBy(0,1200)'); await sleep(1000);"
-#  },
-  vars: {
-    category: "LED & LCD TVs"
-  }
+#  }
 }
