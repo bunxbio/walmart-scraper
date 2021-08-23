813a929df3792c7c79d06ec1c2d38dd84349211e
bunxbio <biokenfigumilang@gmail.com>
2021-08-24 02:16:22 +0700

    modify seeder

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -1,11 +1,11 @@
 pages << {
-  url: 'https://www.walmart.com/browse/new-in-movies/0/0/?_refineresult=true&_be_shelf_id=1522545&search_sort=100&facet=shelf_id:1522545',
+  url: 'https://www.walmart.com/browse/movies-tv-shows/4096?facet=new_releases:Last+90+Days',
   page_type: "listings",
   fetch_type: 'browser',
   force_fetch: true,
   method: "GET",
   headers: {"User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"},
-#  driver: {
-#    code: "await page.evaluate('window.scrollBy(0,1200)'); await sleep(1000); await page.evaluate('window.scrollBy(0,1200)'); await sleep(1000);await page.evaluate('window.scrollBy(0,1200)'); await sleep(1000);await page.evaluate('window.scrollBy(0,1200)'); await sleep(1000);"
-#  }
+  driver: {
+    code: "await page.click('#OyjukVomHUsCcqr'); await sleep(1000);"
+  }
 }
