5eb1253d992a945ab72dc8f7ab1727185f2475cc
bunxbio <biokenfigumilang@gmail.com>
2021-08-24 00:31:49 +0700

    adding config and seeder file

---
+++ seeder/seeder.rb
@@ -0,0 +1,14 @@
+pages << {
+  url: 'https://www.walmart.com/browse/movies-tv-shows/4096?facet=new_releases:Last+90+Days',
+  page_type: "listings",
+  fetch_type: 'browser',
+  force_fetch: true,
+  method: "GET",
+  headers: {"User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"},
+#  driver: {
+#    code: "await page.evaluate('window.scrollBy(0,1200)'); await sleep(1000); await page.evaluate('window.scrollBy(0,1200)'); await sleep(1000);await page.evaluate('window.scrollBy(0,1200)'); await sleep(1000);await page.evaluate('window.scrollBy(0,1200)'); await sleep(1000);"
+#  },
+  vars: {
+    category: "LED & LCD TVs"
+  }
+}
