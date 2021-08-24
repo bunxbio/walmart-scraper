cd0dd71d0b2ac8dcfa610ce481cefeb9519f8538
bunxbio <biokenfigumilang@gmail.com>
2021-08-25 00:34:57 +0700

    modify listings parser

--- parsers/listings.rb
+++ parsers/listings.rb
@@ -1,5 +1,22 @@
 nokogiri = Nokogiri.HTML(content)
 
+click_captha_code = " 
+  await sleep(3000);
+  if ( (await page.$('div#js-global-footer-wrapper form#hf-email-signup-form')) == null){
+    await sleep(5412);
+    if ( (await page.$('iframe[style*=\"display: block\"]')) !== null) {
+      // do things with its content
+      await page.hover('iframe[style*=\"display: block\"]'); await sleep(1428); 
+      // click hold and wait loading new page
+      await Promise.all([
+        page.waitForNavigation(),
+        page.click('iframe[style*=\"display: block\"]', {delay: 9547}),
+      ]);
+    };
+  };
+"
+
+cookies = page['response_cookie']
 products = nokogiri.css('li.Grid-col')
 
 products.each do |product|
@@ -10,6 +27,11 @@ products.each do |product|
 		pages << {
 			url: url,
 			page_type: 'products',
+			fetch_type: 'browser',
+			force_fetch: true,
+			method: "GET",
+			headers: {"User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"},
+			driver: {code: click_captha_code},
 			vars: {
 				url: url
 			}
@@ -18,8 +40,8 @@ products.each do |product|
 end
 
 
-limit_page = 11
-current_page = nokogiri.at_css('.paginator-list > li.active > a.active')
+#limit_page = 11
+#current_page = nokogiri.at_css('.paginator-list > li.active > a.active')
 
 =begin
 if current_page
