100510c8ebf3bcd41ed81b60883540ff9c71a1e2
bunxbio <biokenfigumilang@gmail.com>
2021-08-25 01:15:45 +0700

    fixing listings parser

--- parsers/listings.rb
+++ parsers/listings.rb
@@ -39,24 +39,26 @@ products.each do |product|
 	end
 end
 
-#LIMIT_PAGE = 11
-#current_page = nokogiri.at_css('.paginator-list > li.active > a.active')
 
-=begin
+LIMIT_PAGE = 10
+current_page = nokogiri.at_css('.paginator-list > li.active > a.active')
+
 if current_page
 	current_page = current_page.text.to_i
-end
-
-if current_page && (current_page < limit_page)
-	next_page = "https://www.walmart.com/browse/movies-tv-shows/4096?facet=new_releases%3ALast+90+Days&page=#{current_page + 1}"
-	if next_page
-		pages << {
-			url: next_page,
-			page_type: 'listings',
-			fetch_type: 'browser',
-			force_fetch: true,
-			method: 'GET'
-		}
+	if current_page <= limit_page
+		next_page = "https://www.walmart.com/browse/movies-tv-shows/4096?facet=new_releases%3ALast+90+Days&page=#{current_page + 1}"
+		if next_page
+			pages << {
+				url: next_page,
+				page_type: 'listings',
+				fetch_type: 'browser',
+				force_fetch: true,
+				method: "GET",
+				headers: {"User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"},
+				driver: {
+					code: click_captha_code
+				}
+			}
+		end
 	end
 end
-=end
